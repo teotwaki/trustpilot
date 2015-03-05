900d1d689ed2837c3a39593c47309eef0fc786b2
Sebastian Lauwers <sebastian.lauwers@gmail.com>
2015-03-05 22:45:53 +0100

    Added location information in logging

--- log.c
+++ log.c
@@ -1,6 +1,7 @@
 #include "log.h"
 
-void _log(FILE * fp, char const * tag, char const * format, ...) {
+void _log(FILE * fp, char const * tag, char const * filename, int line,
+		char const * format, ...) {
 	time_t now;
 	char iso_buffer[25];
 	struct tm * tm_info;
@@ -8,7 +9,7 @@ void _log(FILE * fp, char const * tag, char const * format, ...) {
 	tm_info = localtime(&now);
 	strftime(iso_buffer, 25, "%FT%T%z", tm_info);
 
-	fprintf(fp, "%s - [%s] - ", iso_buffer, tag);
+	fprintf(fp, "%s - [%s] - %s:%d - ", iso_buffer, tag, filename, line);
 
 	va_list ap;
 	va_start(ap, format);
--- log.h
+++ log.h
@@ -5,30 +5,31 @@
 #include <stdio.h>
 #include <time.h>
 
-void _log(FILE * fp, char const * tag, char const * format, ...);
+void _log(FILE * fp, char const * tag, char const * filename, int line,
+		char const * format, ...);
 
 #define DEBUG(msg) \
-	_log(stdout, "DEBUG", "%s", msg)
+	_log(stdout, "DEBUG", __FILE__, __LINE__, "%s", msg)
 
 #define VDEBUG(format, ...) \
-	_log(stdout, "DEBUG", format, __VA_ARGS__)
+	_log(stdout, "DEBUG", __FILE__, __LINE__, format, __VA_ARGS__)
 
 #define INFO(msg) \
-	_log(stdout, "INFO", "%s", msg)
+	_log(stdout, "INFO", __FILE__, __LINE__, "%s", msg)
 
 #define VINFO(format, ...) \
-	_log(stdout, "INFO", format, __VA_ARGS__)
+	_log(stdout, "INFO", __FILE__, __LINE__, format, __VA_ARGS__)
 
 #define WARN(msg) \
-	_log(stdout, "WARN", "%s", msg)
+	_log(stdout, "WARN", __FILE__, __LINE__, "%s", msg)
 
 #define VWARN(format, ...) \
-	_log(stdout, "WARN", format, __VA_ARGS__)
+	_log(stdout, "WARN", __FILE__, __LINE__, format, __VA_ARGS__)
 
 #define ERROR(msg) \
-	_log(stderr, "ERROR", "%s", msg)
+	_log(stderr, "ERROR", __FILE__, __LINE__, "%s", msg)
 
 #define VERROR(format, ...) \
-	_log(stderr, "ERROR", format, __VA_ARGS__)
+	_log(stderr, "ERROR", __FILE__, __LINE__, format, __VA_ARGS__)
 
 #endif
