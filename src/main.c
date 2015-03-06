980989b841cd43f5dc926ab7cf4c81131b3de39e
Sebastian Lauwers <sebastian.lauwers@gmail.com>
2015-03-06 12:48:45 +0100

    Allow host specification via environment variable

--- src/main.c
+++ src/main.c
@@ -1,14 +1,29 @@
 #include "main.h"
 
+char * get_host(void) {
+	char const * host = getenv("SOLVER_HOST");
+
+	if (host == NULL) {
+		WARN("Environment variable SOLVER_HOST not set.");
+		host = "tcp://" HOST ":" PORT;
+		VINFO("Defaulting to %s.", host);
+	}
+
+	return strdup(host);
+}
+
 int main() {
-	solver_t * solver = solver_init("tcp://" HOST ":" PORT);
+	char * solver_host = get_host();
+	solver_t * solver = solver_init(solver_host);
 
 	if (solver == NULL) {
 		ERROR("Solver initialisation failed.");
+		free(solver_host);
 		exit(EXIT_FAILURE);
 	}
 
 	solver_destroy(solver);
+	free(solver_host);
 
 	exit(EXIT_SUCCESS);
 }
--- src/main.h
+++ src/main.h
@@ -1,11 +1,15 @@
 #ifndef MAIN_H__
 #define MAIN_H__
 
+#include <stdio.h>
+
 #include "solver.h"
 #include "log.h"
 
 #define HOST "localhost"
 #define PORT "20123"
+
+char * get_host(void);
 
 int main(void);
 
